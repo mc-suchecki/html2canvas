965961377dbbc12159fea7c6ad7319608e8039a3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-02 12:43:10 +0000

    Deploy html2canvas.hertzen.com to github.com/niklasvh/html2canvas.git:gh-pages

---
+++ path---index-d0d1c1bf7294832277d6.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{451:function(t,a){t.exports={data:{site:{siteMetadata:{packageSize:39540}}},pathContext:{}}}});
+//# sourceMappingURL=path---index-d0d1c1bf7294832277d6.js.map
